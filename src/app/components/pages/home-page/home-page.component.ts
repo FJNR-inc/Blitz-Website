55df23c379247c04e13abcb944c474ea238af45d
RignonNoel <rignon.noel@gmail.com>
2018-07-16 14:44:18 -0400

    add partners on homepage

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -13,9 +13,110 @@ import { Workplace } from '../../../models/workplace';
 export class HomePageComponent implements OnInit {
 
   workplaceList: Workplace[];
+  sponsorsList = [
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners1.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners2.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners3.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners4.png',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners5.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners6.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners7.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners8.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners9.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners10.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners11.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners12.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners13.gif',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners14.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners15.png',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners16.png',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners17.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners18.png',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners19.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners20.gif',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners21.jpg',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners22.png',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners23.png',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners24.png',
+      'name': '',
+    },
+    {
+      'imageUrl': '../../../../assets/images/temp/partners/partners25.jpg',
+      'name': '',
+    }
+  ];
 
   constructor(private workplaceService: WorkplaceService) { }
-
 
   ngOnInit() {
     this.workplaceService.list().subscribe(
