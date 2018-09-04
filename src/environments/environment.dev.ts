37b1fa4e7a95edf73f719197c317d137d82d7e86
Noel RIGNON <noel.rignon@fjnr.ca>
2018-09-03 21:38:32 -0400

    env for dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   default_language: 'fr',
-  url_base_api: 'https://api.thesez-vous.org',
+  url_base_api: 'https://api-dev.thesez-vous.org',
   token_paysafe: 'T1QtMjMxODcwOkItcWEyLTAtNWI4NmUxMDktMC0zMDJjMDIxNDJiMWI4YjNjZmMyNmM0YTc3ZTUzNTI0YjYzNzEwMzU3YWE1NmYzNWMwMjE0NjM5N2ZhZTJiMDZjNjFiYmFhNGQ1NGZjNDE2MjlhOGIxNzVmOTRhNw==',
   paths_api: {
     activation: '/users/activate',
