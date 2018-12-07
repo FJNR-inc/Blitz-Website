3a03b10bb375ecf585cd74766f978caa4c39ebbd
Noel RIGNON <noel.rignon@fjnr.ca>
2018-12-07 11:33:54 -0500

    stop support of my-modal in modal service

--- src/app/services/my-modal/my-modal.service.ts
+++ src/app/services/my-modal/my-modal.service.ts
@@ -1,16 +1,15 @@
-import { MyModalComponent } from '../../components/my-modal/my-modal.component';
 import { Injectable } from '@angular/core';
 import {NtModalComponent} from '../../components/nt-modal/nt-modal.component';
 
 @Injectable()
 export class MyModalService {
-  map: Map<string, MyModalComponent|NtModalComponent> = new Map;
+  map: Map<string, NtModalComponent> = new Map;
 
-  get(v: string): MyModalComponent|NtModalComponent {
+  get(v: string): NtModalComponent {
     return this.map.get(v);
   }
 
-  set(key: string, v: MyModalComponent|NtModalComponent): void {
+  set(key: string, v: NtModalComponent): void {
     this.map.set(key, v);
   }
 }
