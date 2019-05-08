ba2295f1e24fc8ec803cd7379d0f12f0be292625
willy <willkoua@yahoo.fr>
2019-05-08 14:35:03 -0400

    add to update university and email in profile

---
+++ src/app/components/pages/validate-email/validate-email.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {map} from 'rxjs/operators';
+
+import {UserService} from '../../../services/user.service';
+import {User} from '../../../models/user';
+import {ProfileService} from '../../../services/profile.service';
+import {AuthenticationService} from '../../../services/authentication.service';
+
+
+@Component({
+  selector: 'app-profile-activate',
+  templateUrl: './validate-email.component.html',
+  styleUrls: ['./validate-email.component.scss']
+})
+export class ValidateEmailComponent implements OnInit {
+  token: string;
+  profile: User;
+  flagSuccess = false;
+
+  constructor(
+    private userService: UserService,
+    private profileService: ProfileService,
+    private authenticationService: AuthenticationService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.token = params['token'];
+    });
+
+    this.userService.activateToken(this.token)
+      .pipe(map(result => result.user))
+      .subscribe(
+        value => {
+          this.authenticationService.setProfile(value);
+          this.profile = new User(value)
+          this.flagSuccess = true;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+}
