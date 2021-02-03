817735f7bd601b1f153b1971855cd43cdda6cf46
Rignon Noël <noel.rignon@fjnr.ca>
2021-02-02 20:50:23 -0500

    fix(retreat-invitation): hide coupon message when not needed (#180)

--- src/app/components/pages/hidden-retreat/hidden-retreat.component.ts
+++ src/app/components/pages/hidden-retreat/hidden-retreat.component.ts
@@ -42,7 +42,11 @@ export class HiddenRetreatComponent implements OnInit, OnDestroy {
           if (data.count > 0) {
             this.invitation = data.results[0];
             this.retreat = new Retreat(this.invitation.retreat_detail);
-            this.coupon = new Coupon(this.invitation.coupon_detail);
+            if (this.invitation.coupon_detail) {
+              this.coupon = new Coupon(this.invitation.coupon_detail);
+            } else {
+              this.coupon = null;
+            }
           } else {
             this.router.navigate(['/404']).then();
           }
